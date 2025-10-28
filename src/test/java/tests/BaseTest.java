c1d6f835c13984b5ecb8ede67d9fc3d0d6497202
FilinTim <ghuthdls@mail.ru>
2025-10-28 14:41:41 +0300

    Initial commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
